b6ed200164af6e4452dc4e75e0a083d497aa5433
raushan1 <67915231+raushan1@users.noreply.github.com>
2020-07-07 15:32:38 +0530

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,18 +1,12 @@
 from hello import toyou, add, subtract
-
-
 def setup_function(function):
   print("Running Setup: %s" % function._name_)
   function.x = 10
-
 def teardown_function(function):
   print("Running Teardown: %s" % function._name_)
   del function.x
-
-
 #### Run to see failed test
 #def test_hello_add():
 # assert add(test_hello_add.x) == 12
-
 def test_hello_subtract():
   assert subtract(test_hello_subtract.x) == 9
